e88ab5d656e496a8833c8e3fca612724fa33c057
guallo <eacuesta1986@gmail.com>
2019-06-19 20:50:32 -0400

    handling of non-required fields and SSL support for development

--- agent_jobs_service/api/models.py
+++ agent_jobs_service/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Job(models.Model):
     uuid = models.CharField(max_length=36, primary_key=True)
     cmd = models.TextField()
-    directory = models.CharField(max_length=128)
-    user = models.CharField(max_length=32)
-    b64payload = models.TextField()
-    extra_env_json = models.TextField()
+    directory = models.CharField(max_length=128, blank=True)
+    user = models.CharField(max_length=32, blank=True)
+    b64payload = models.TextField(blank=True)
+    extra_env_json = models.TextField(blank=True)
--- agent_jobs_service/settings.py
+++ agent_jobs_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sslserver',  # only for development
     'rest_framework',
     'agent_jobs_service.api',
 ]
