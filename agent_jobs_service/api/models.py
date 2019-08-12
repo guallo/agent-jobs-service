468ccb280c885c3311f60b5d45ff8a4d6f82dd6b
guallo <eacuesta1986@gmail.com>
2019-08-11 21:25:47 -0400

    changed Job.uuid model field from CharField to UUIDField

--- agent_jobs_service/api/models.py
+++ agent_jobs_service/api/models.py
@@ -1,8 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Job(models.Model):
-    uuid = models.CharField(max_length=36, primary_key=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     cmd = models.TextField()
     directory = models.CharField(max_length=128, blank=True)
     user = models.CharField(max_length=32, blank=True)
