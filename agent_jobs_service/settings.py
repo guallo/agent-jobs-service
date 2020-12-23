ffd672b4f78537526b5b21e76540c750c9dc6ccb
guallo <eacuesta1986@gmail.com>
2020-12-23 09:10:27 -0500

    unpack base DATABASES in settings.py

--- agent_jobs_service/settings.py
+++ agent_jobs_service/settings.py
@@ -12,6 +12,7 @@ INSTALLED_APPS += [
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    **DATABASES,
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
